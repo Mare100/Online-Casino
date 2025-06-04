f24e5f1769d16b0b6a8cfc16b593ef9f81cd2960
ME100 <eichbergermarc291@gmail.com>
2025-06-04 16:42:25 +0200

    Dice Roller: Farbwechsel des Butto fertig

--- lib/dice.dart
+++ lib/dice.dart
@@ -20,7 +20,13 @@ class _DiceRollerState extends State<DiceRoller> {
   int currentDiceRoll = 2;
   int choice=0;
   bool isButtonActive = true;
-  Color _iconColor = Colors.black;
+
+  Color _iconColor1 = Colors.black;
+  Color _iconColor2 = Colors.black;
+  Color _iconColor3 = Colors.black;
+  Color _iconColor4 = Colors.black;
+  Color _iconColor5 = Colors.black;
+  Color _iconColor6 = Colors.black;
 
 
   final TextEditingController _controller = TextEditingController();
@@ -91,41 +97,43 @@ class _DiceRollerState extends State<DiceRoller> {
     if(choice== 0  ){
       return Text("");
     }else
-      if(input== 0  ){
-        return Text("");
+    if(input== 0  ){
+      return Text("");
     }else{
       if (currentDiceRoll == choice) {
         return
           SizedBox(
-            height: 50,
-            child:
-          Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: isButtonActive
-                  ? () {
-                setState(() {
-                  state.sharedCounter = state.sharedCounter + input*6; // Coins Counter erhöhen
-                  isButtonActive = false; //Button deaktivieren, damit nur 1 mal Coins eingelöst werden können;
-                });
-              }: null,
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: Colors.lightBlueAccent,
-                textStyle: const TextStyle(
-                  fontSize: 28,
-                ),
-              ),
-              child: const Text('Cash In'),
-            ),
+              height: 50,
+              child:
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: isButtonActive
+                        ? () {
+                      setColorBack();
+                      //setColorBack();
+                      setState(() {
+                        state.sharedCounter = state.sharedCounter + input*6; // Coins Counter erhöhen
+                        isButtonActive = false; //Button deaktivieren, damit nur 1 mal Coins eingelöst werden können;
+                      });
+                    }: null,
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.lightBlueAccent,
+                      textStyle: const TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                    child: const Text('Cash In'),
+                  ),
 
-          ],
-          ));
+                ],
+              ));
       } else {
         return SizedBox(
-          height: 50,
-          child: Text(""));
+            height: 50,
+            child: Text(""));
       }
     }
   }
@@ -133,44 +141,50 @@ class _DiceRollerState extends State<DiceRoller> {
   Widget inputForm(){
     return
 
-    Column(
+      Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-      Padding(
-      padding: const EdgeInsets.all(16.0),
-    child: Column(
-    children: [
-      SizedBox(height: 30,),
-      diceButtons(),
-    SizedBox(height: 30,),
-        SizedBox(
-          width: 300,
-          child:
-              Column(children: [
-    TextField(
-    controller: _controller,
-    decoration: InputDecoration(labelText: 'Wie viel setzt du?'),
-    ),
-    SizedBox(height: 20),
-    ElevatedButton(
-    onPressed: _submit,
-    child: Text('Submit'),
-    ),
-    SizedBox(height: 20),
-       ]) )
-    ],
-    ),
-    )
-    ]
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 30,),
+                  diceButtons(),
+                  SizedBox(height: 30,),
+                  SizedBox(
+                      width: 300,
+                      child:
+                      Column(children: [
+                        TextField(
+                          controller: _controller,
+                          decoration: InputDecoration(labelText: 'Wie viel setzt du?'),
+                        ),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: _submit,
+                          child: Text('Submit'),
+                        ),
+                        SizedBox(height: 20),
+                      ]) )
+                ],
+              ),
+            )
+          ]
       );
 
   }
-
-  void changeColor(){
+  void setColorBack(){
     setState(() {
-      _iconColor = Colors.grey;
+      _iconColor1 = Colors.black;
+      _iconColor2 = Colors.black;
+      _iconColor3 = Colors.black;
+      _iconColor4 = Colors.black;
+      _iconColor5 = Colors.black;
+      _iconColor6 = Colors.black;
     });
   }
+
+
 
 
   Widget diceButtons(){
@@ -178,47 +192,103 @@ class _DiceRollerState extends State<DiceRoller> {
     return
       Column(children: [
         Text("Wähle eine Zahl auf die du dein Geld setzt"),
-      Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        IconButton(
-          icon: SizedBox(height: 45, child: Image.asset('assets/images/dice1.png')),
-          onPressed: () {
-            _setChoice(1);          },
-        ),
-        IconButton(
-          icon: SizedBox(height: 45, child: Image.asset('assets/images/dice2.png')),
-          onPressed: () {
-            _setChoice(2);          },
-        ),
-        IconButton(
-          icon: SizedBox(height: 45, child: Image.asset('assets/images/dice3.png')),
-          onPressed: () {
-            _setChoice(3);          },
-        ),
-        IconButton(
-          icon: SizedBox(height: 45, child: Image.asset('assets/images/dice4.png', color: _iconColor),),
-          onPressed: () {
-            _setChoice(4);
-            changeColor();
-          },
-        ),
-        IconButton(
-          icon: SizedBox(height: 45, child: Image.asset('assets/images/dice5.png')),
-          onPressed: () {
-            _setChoice(5);
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: SizedBox(height: 45, child: Image.asset('assets/images/dice1.png', color: _iconColor1)),
+              onPressed: () {
+                _setChoice(1);
+                setState(() {
+                  _iconColor1 = Colors.grey;
 
-          },
-        ),
-        IconButton(
-          icon: SizedBox(height: 45, child: Image.asset('assets/images/dice6.png')),
-          onPressed: () {
-            _setChoice(6);
-          },
-        ),
+                  _iconColor2 = Colors.black;
+                  _iconColor3 = Colors.black;
+                  _iconColor4 = Colors.black;
+                  _iconColor5 = Colors.black;
+                  _iconColor6 = Colors.black;
+                });
+              },
+            ),
+            IconButton(
+              icon: SizedBox(height: 45, child: Image.asset('assets/images/dice2.png', color: _iconColor2)),
+              onPressed: () {
+                _setChoice(2);
+                setState(() {
+                  _iconColor2 = Colors.grey;
 
-      ],
-    ),
+                  _iconColor1 = Colors.black;
+                  _iconColor3 = Colors.black;
+                  _iconColor4 = Colors.black;
+                  _iconColor5 = Colors.black;
+                  _iconColor6 = Colors.black;
+                });
+              },
+            ),
+            IconButton(
+              icon: SizedBox(height: 45, child: Image.asset('assets/images/dice3.png', color: _iconColor3)),
+              onPressed: () {
+                _setChoice(3);
+                setState(() {
+                  _iconColor3 = Colors.grey;
+
+                  _iconColor1 = Colors.black;
+                  _iconColor2 = Colors.black;
+                  _iconColor4 = Colors.black;
+                  _iconColor5 = Colors.black;
+                  _iconColor6 = Colors.black;
+                });
+              },
+            ),
+            IconButton(
+              icon: SizedBox(height: 45, child: Image.asset('assets/images/dice4.png', color: _iconColor4),),
+              onPressed: () {
+                _setChoice(4);
+                setState(() {
+                  _iconColor4 = Colors.grey;
+
+                  _iconColor1 = Colors.black;
+                  _iconColor2 = Colors.black;
+                  _iconColor3 = Colors.black;
+                  _iconColor5 = Colors.black;
+                  _iconColor6 = Colors.black;
+                });
+              },
+            ),
+            IconButton(
+              icon: SizedBox(height: 45, child: Image.asset('assets/images/dice5.png',color: _iconColor5)),
+              onPressed: () {
+                _setChoice(5);
+                setState(() {
+                  _iconColor5 = Colors.grey;
+
+                  _iconColor1 = Colors.black;
+                  _iconColor2 = Colors.black;
+                  _iconColor3 = Colors.black;
+                  _iconColor4 = Colors.black;
+                  _iconColor6 = Colors.black;
+                });
+
+              },
+            ),
+            IconButton(
+              icon: SizedBox(height: 45, child: Image.asset('assets/images/dice6.png', color: _iconColor6)),
+              onPressed: () {
+                _setChoice(6);
+                setState(() {
+                  _iconColor6 = Colors.grey;
+
+                  _iconColor1 = Colors.black;
+                  _iconColor2 = Colors.black;
+                  _iconColor3 = Colors.black;
+                  _iconColor4 = Colors.black;
+                  _iconColor5 = Colors.black;
+                });
+              },
+            ),
+
+          ],
+        ),
 
       ],);
   }
@@ -240,48 +310,47 @@ class _DiceRollerState extends State<DiceRoller> {
     final state = AppState();
 
     return Scaffold(
-    appBar: AppBar(
-      leading: IconButton(
-        iconSize: 20,
-        icon: Icon(Icons.arrow_back),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const FirstPage()),
-          );
-        },
-      ),
-      title: const Text('Second Page'),
-      actions: <Widget>[
-        Icon(Icons.currency_bitcoin),
-        Text("${state.sharedCounter}"),
-        SizedBox(width: 20,)
-      ],),
-    body:
-        Column(
-        children: [
-        SizedBox(height: 60,),
-        //check(),
-        show(),
-        const SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: roleDice,
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.black,
-            backgroundColor: Colors.lightBlueAccent,
-            textStyle: const TextStyle(
-              fontSize: 28,
-            ),
+        appBar: AppBar(
+          leading: IconButton(
+            iconSize: 20,
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const FirstPage()),
+              );
+            },
           ),
-          child: const Text('Role Dice'),
-        ),
-        //Text('Current number = $currentDiceRoll!'),
-        win(),
-          inputForm(),
-          //Text("${choice}"),
-        ]
+          title: const Text('Second Page'),
+          actions: <Widget>[
+            Icon(Icons.currency_bitcoin),
+            Text("${state.sharedCounter}"),
+            SizedBox(width: 20,)
+          ],),
+        body:
+        Column(
+            children: [
+              SizedBox(height: 60,),
+//check(),
+              show(),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: roleDice,
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.lightBlueAccent,
+                  textStyle: const TextStyle(
+                    fontSize: 28,
+                  ),
+                ),
+                child: const Text('Role Dice'),
+              ),
+//Text('Current number = $currentDiceRoll!'),
+              win(),
+              inputForm(),
+//Text("${choice}"),
+            ]
         )
 
     );
   }
-}
